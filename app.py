7b5395668c532ddd6d86f00950eec8738ea22340
Leonardo de Moura Chacarolli <leonardochacarolli@gmail.com>
2024-07-04 22:02:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,54 @@
-print('Sabor express')
+import os
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair')
+def exibir_nome_programa():
+    print('''
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+        ''')
+
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o funcionamento do App\n')
+
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    input('Digite algo para voltar ao menu principal\n')
+    main()
+
+def escolher_opcoes():
+
+    try:
+        opcao_escolhida = int(input('Digite uma opção: \n'))
+
+        if opcao_escolhida == 1:
+            print(f'Voce escolheu a opção: {opcao_escolhida}. Cadastrar Restaurante')
+        elif opcao_escolhida == 2:
+            print(f'Voce escolheu a opção: {opcao_escolhida}. Listar Restaurante')
+        elif opcao_escolhida == 3:
+            print(f'Voce escolheu a opção: {opcao_escolhida}. Ativar Restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+        
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('cls')
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
